c1a383022636a3828e6ac7f12c7f713cc686ab0e
Dominik Oreskovic <domoreskovic@gmail.com>
2018-07-10 18:16:34 +0200

    Making insertion sort more descriptive

--- src/algorithms/sorting/insertion/Demo.java
+++ src/algorithms/sorting/insertion/Demo.java
@@ -4,8 +4,9 @@ import java.util.Arrays;
 
 public class Demo {
     public static void main(String[] args) {
-        int[] arr = {5, 1, 3};
+        int[] arr = {5, 1, 3, 3, 6, 3, 2};
+        System.out.println("Input: " + Arrays.toString(arr));
         InsertionSort.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        System.out.println("Output: " + Arrays.toString(arr));
     }
 }
--- src/algorithms/sorting/insertion/InsertionSort.java
+++ src/algorithms/sorting/insertion/InsertionSort.java
@@ -3,12 +3,17 @@ package algorithms.sorting.insertion;
 public class InsertionSort {
     public static void sort(int[] arr) {
         for(int j = 1; j < arr.length; j++) {
+            // "key" is the element whose position in the subarray is about to be determined
             int key = arr[j];
+
+            // once this block is done, "i" will be the index of an element smaller than "key" or -1
             int i = j - 1;
             while(i >= 0 && arr[i] > key) {
                 arr[i+1] = arr[i];
                 i--;
             }
+
+            // place "key" next to the element on position "i"
             arr[i + 1] = key;
         }
     }
