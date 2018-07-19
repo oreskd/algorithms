6fb121f14015edf53fd2627e129f35a98a83f6ce
Dominik Oreskovic <domoreskovic@gmail.com>
2018-07-19 11:52:05 +0200

    Adding pair of integers as a utility data structure

---
+++ src/datastructures/utils/IntPair.java
@@ -0,0 +1,15 @@
+package datastructures.utils;
+
+/**
+ * Utility class representing a pair of integers, (x, y). There are no restrictions for x or y.
+ */
+public class IntPair {
+    // intentionally making these variables public, as there is no added value in providing getters/setters
+    public int x;
+    public int y;
+
+    public IntPair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
