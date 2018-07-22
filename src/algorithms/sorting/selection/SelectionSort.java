a075763d974797e3b4db7d2a9c98136df317ba9e
Dominik Oreskovic <domoreskovic@gmail.com>
2018-07-22 20:48:08 +0200

    Adding optimized version of selection sort + descriptive comments for both selection sorts

--- src/algorithms/sorting/selection/SelectionSort.java
+++ src/algorithms/sorting/selection/SelectionSort.java
@@ -3,6 +3,7 @@ package algorithms.sorting.selection;
 public class SelectionSort {
     public static void sort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
+            // find the smallest element in the subarray
             int minIndex = i;
             for (int j = i+1; j < arr.length; j++) {
                 if (arr[j] < arr[minIndex]) {
@@ -10,9 +11,43 @@ public class SelectionSort {
                 }
             }
 
+            // switch the smallest element with the first element of the subarray
             int helper = arr[i];
             arr[i] = arr[minIndex];
             arr[minIndex] = helper;
+        }
+    }
+
+    public static void sort_both_sides(int[] arr) {
+        int last_index = arr.length - 1;
+        for (int i = 0; i <= last_index; i++) {
+            // find the smallest and the largest element in the subarray
+            int minIndex = i;
+            int maxIndex = i;
+            for (int j = i+1; j <= last_index; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                } else if (arr[j] > arr[maxIndex]) {
+                    maxIndex = j;
+                }
+            }
+
+            // switch the smallest element with the first element of the subarray
+            int helper = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = helper;
+
+            // in case largest element got moved by the previous change, update maxIndex
+            if (maxIndex == i) {
+                maxIndex = minIndex;
+            }
+
+            // switch the largest element with the last element of the subarray
+            helper = arr[last_index];
+            arr[last_index] = arr[maxIndex];
+            arr[maxIndex] = helper;
+
+            last_index--;
         }
     }
 }
