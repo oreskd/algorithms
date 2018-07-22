c6ed0b030795bb3849fb91e83d303fe914763119
Dominik Oreskovic <domoreskovic@gmail.com>
2018-07-22 20:26:54 +0200

    Adding selection sort

---
+++ src/algorithms/sorting/selection/SelectionSort.java
@@ -0,0 +1,18 @@
+package algorithms.sorting.selection;
+
+public class SelectionSort {
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int helper = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = helper;
+        }
+    }
+}
