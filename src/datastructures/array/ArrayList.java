2f22feaf34f8abf9dcc89b23e2e80e46e7aa8254
Dominik Oreskovic <domoreskovic@gmail.com>
2018-07-30 19:48:35 +0200

    Adding few descriptive comments for ArrayList

--- src/datastructures/array/ArrayList.java
+++ src/datastructures/array/ArrayList.java
@@ -7,7 +7,7 @@ public class ArrayList<T> {
     private int size;
 
     public ArrayList() {
-        arr = (T[]) new Object[1];
+        arr = (T[]) new Object[1];  // initial size is 1; later gets increased/decreased to 2, 4, 8, 16... as needed
     }
 
     public int size() {
@@ -26,7 +26,7 @@ public class ArrayList<T> {
 
     public void add(int index, T element) {
         if (size >= arr.length) {
-            resize();
+            resize();  // increase the size in order to be able to add the new element
         }
         System.arraycopy(arr, index, arr, index+1, size-index);
         arr[index] = element;
@@ -36,10 +36,10 @@ public class ArrayList<T> {
     public T remove(int index) {
         T element = arr[index];
         System.arraycopy(arr, index+1, arr, index, size-index-1);
-        arr[size-1] = null;
+        arr[size-1] = null;  // after copying, this element was left unchanged and therefore should be unset
         size--;
         if (3*size <= arr.length) {
-            resize();
+            resize();  // reduce the size to not use more memory than needed
         }
         return element;
     }
@@ -50,6 +50,7 @@ public class ArrayList<T> {
         System.arraycopy(arr, 0, newArr, 0, size);
         arr = newArr;
     }
+
 
     // this is not related to the data structure itself, but is useful for Demo purposes
     @Override
